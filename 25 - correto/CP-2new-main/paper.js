62938ccfd941553f6f3821fa1b039ea413c9be0f
jOBISCREIDO <72766169+jOBISCREIDO@users.noreply.github.com>
2022-02-03 22:38:07 -0300

    Add files via upload

---
+++ 25 - correto/CP-2new-main/paper.js
@@ -0,0 +1,76 @@
+/*class paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0,
+			density:1.2
+
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.image=loadImage("paper.png");
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var paperpos=this.body.position;		
+
+			push()
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			fill(255,0,255)
+			imageMode(CENTER);
+			image(this.image, 0,0,this.r, this.r)
+			//ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
+
+}*/
+
+class paper {
+
+    
+    constructor(x, y, r) {
+
+        var opitions = {
+
+            isStatic:false,
+            restitution: 0.3,
+            friction: 0,
+            density: 1.2
+
+        }
+        
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.image = loadImage("paper.png");
+        this.body = Bodies.circle(this.x, this.y, (this.r-20)/2,  opitions);
+        World.add(world, this.body);
+
+    }
+
+    display() 
+    {
+
+
+        var paperPos = this.body.position;
+        push()
+        translate(paperPos.x, paperPos.y);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.r, this.r);
+        pop();
+
+    }
+
+}
